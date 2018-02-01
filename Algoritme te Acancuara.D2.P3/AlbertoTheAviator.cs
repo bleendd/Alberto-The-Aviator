6d57ede5aa63f9d43298791554ad4848da3d5b49
Blend <Blend@BLENDIPC>
2018-02-01 10:58:05 +0100

    Add project files.

---
+++ Algoritme te Acancuara.D2.P3/AlbertoTheAviator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritme_te_Acancuara.D2.P3
+{
+    public class AlbertoTheAviator
+    {
+        public static int MaximumFlights(int fMax, int[] duration, int[] refuel)
+        {
+            int nrMisioneve = duration.Length;
+            List<HashSet<int>>[] M = new List<HashSet<int>>[fMax + 1];
+            for (var i = 0; i < M.Length; i++)
+            {
+                M[i] = new List<HashSet<int>>();
+            }
+
+            for (int f = 1; f <= fMax; f++)
+            {
+                for (int i = 0; i < nrMisioneve; i++)
+                {
+                    if (f < duration[i])
+                    {
+                        continue;
+                    }
+
+                    M[f].Add(new HashSet<int> { i });
+
+                    var afterMission = f - (duration[i] - refuel[i]);
+                    for (var j = 1; j <= afterMission; j++)
+                    {
+                        foreach (var set in M[j])
+                        {
+                            var newSet = new HashSet<int>(set);
+                            newSet.Add(i);
+                            if (!M[f].Any(solution => newSet.SetEquals(solution)))
+                            {
+                                M[f].Add(newSet);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return M[fMax].Count == 0 ? 0 : M[fMax].Max(x => x.Count);
+        }
+    }
+}
---
+++ Algoritme te Acancuara.D2.P3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritme_te_Acancuara.D2.P3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = AlbertoTheAviator.MaximumFlights(10, new[] { 10 }, new[] { 0 });
+            Console.WriteLine($"0) f=10 d=[10], r=[0] => {result}");
+            result = AlbertoTheAviator.MaximumFlights(10, new[] { 8, 4 }, new[] { 0, 2 });
+            Console.WriteLine($"1) f=10 d=[8,4], r=[0,2] => {result}");
+            result = AlbertoTheAviator.MaximumFlights(12, new[] { 4, 8, 2, 1 }, new[] { 2, 0, 0, 0 });
+            Console.WriteLine($"2) f=12 d=[4,8,2,1], r=[2,0,0,0] => {result}");
+            result = AlbertoTheAviator.MaximumFlights(9, new[] { 4, 6 }, new[] { 0, 1 });
+            Console.WriteLine($"3) f=9 d=[4,6], r=[0,1] => {result}");
+            result = AlbertoTheAviator.MaximumFlights(100, new[] { 101 }, new[] { 100 });
+            Console.WriteLine($"4) f=100 d=[101], r=[100] => {result}");
+            result = AlbertoTheAviator.MaximumFlights(1947, new[] { 2407, 2979, 1269, 2401, 3227, 2230, 3991, 2133, 3338, 356, 2535, 3859, 3267, 365 }, new[] { 2406, 793, 905, 2400, 1789, 2229, 1378, 2132, 1815, 355, 72, 3858, 3266, 364 });
+            Console.WriteLine($"5) f=1947 d=[2407,2979,1269,2401,3227,2230,3991,2133,3338,356,2535,3859,3267,365], r=[2406,793,905,2400,1789,2229,1378,2132,1815,355,72,3858,3266,364] => {result}");
+            Console.ReadLine();
+        }
+    }
+}
